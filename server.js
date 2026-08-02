cb0d4ba464ec57b2d50e97fa46d4ccd82d0c22ea
Rey <GuerrieriEvelin211@gmail.com>
2026-08-02 17:28:17 +0800

    fix: increase payload limit

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
