00044494ab22cae9bbdc826f0046c1854a1365d7
Rey <guerrierievelin211@gmail.com>
2026-08-08 21:22:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
