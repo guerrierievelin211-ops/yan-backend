1969ec75ae03fa6103efe58e88db0940fe90480e
Rey <guerrierievelin211@gmail.com>
2026-08-08 22:00:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
